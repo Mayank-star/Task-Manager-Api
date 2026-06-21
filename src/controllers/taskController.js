3dda2973342f97af8f1031280243ff401a5d22e0
Mayank Kumar <mayankkumarcse@gmail.com>
2026-06-22 00:25:00 +0530

    add pagnation in get all task api

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -30,19 +30,62 @@ exports.createTask = async (req, res) => {
 
 exports.getAllTasks = async (req, res) => {
   try {
-    const [tasks] = await db.query(
-      `SELECT * FROM TASKS WHERE user_id = ?
-       ORDER BY created_at DESC 
-      `,
-      [req.user.id],
-    );
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+    const search = req.query.search || "";
+    const status = req.query.status || "";
+    const offset = (page - 1) * limit;
 
+    let query = `SELECT * FROM tasks WHERE user_id = ?`;
+    const values = [req.user.id];
+
+    let countQuery = `SELECT COUNT(*) as total from tasks WHERE user_id = ?`;
+    const countValues = [req.user.id];
+
+    // search
+    if (search) {
+      query += ` AND title LIKE ?`;
+      values.push(`%${search}%`);
+
+      countQuery += ` AND title LIKE ?`;
+      countValues.push(`%${search}%`);
+    }
+
+    // status
+    if (status) {
+      query += ` AND status = ?`;
+      values.push(status);
+
+      countQuery += ` AND status = ?`;
+      countValues.push(status);
+    }
+
+    query += ` 
+      ORDER BY created_at DESC
+      LIMIT ?
+      OFFSET ?`;
+    values.push(limit);
+    values.push(offset);
+
+    const [tasks] = await db.query(query, values);
+    const [[countResult]] = await db.query(countQuery,countValues);
+    const totalRecords = countResult.total;
+    const totalPages = Math.ceil(totalRecords / limit);
+    // console.log(query)
+    // console.log(req.query);
+    // console.log("status =>", status);
     return res.status(200).json({
       success: true,
-      count: tasks.length,
+      currentPage: page,
+      totalRecords,
+      totalPages,
+      hasNextPage: page < totalPages,
+      hasPreviousPage: page > 1,
+      limit,
       tasks,
     });
   } catch (error) {
+      console.log(error);
     res.status(500).json({
       success: false,
       message: "Server Error",
@@ -142,7 +185,7 @@ exports.deleteTask = async (req, res) => {
     const taskId = req.params.id;
 
     const [result] = await db.query(
-            `
+      `
         DELETE FROM tasks
         WHERE id = ?
         AND user_id = ?
@@ -162,7 +205,7 @@ exports.deleteTask = async (req, res) => {
       message: "Task Deleted",
     });
   } catch (error) {
-    console.log(error);
+    // console.log(error);
 
     res.status(500).json({
       success: false,
